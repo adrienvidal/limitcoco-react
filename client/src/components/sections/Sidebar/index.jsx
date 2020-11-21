e355b7d3646dc4132f33e89f941925e233b0bf30
Adrien Vidal <adriensg.vidal@gmail.com>
2020-11-21 18:31:47 +0100

    init sidebar

--- client/src/components/sections/Sidebar/index.jsx
+++ client/src/components/sections/Sidebar/index.jsx
@@ -2,11 +2,19 @@ import React from 'react'
 import style from './index.module.scss'
 
 const Sidebar = () => {
-	return (
-		<div className={style.main}>
-			<h2>Sidebar</h2>
-		</div>
-	)
+  return (
+    <div className={style.main}>
+      <h2>LimitCoco</h2>
+
+      <div className={style.players}>
+        <div className={style.player}>
+          <span className='name'>Player 1</span>
+          <span className='score'>3</span>
+          <span className='turn'>wait</span>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default Sidebar
