35f468ba6dd9ce7b45120b47befd1c30184b73b8
Adrien Vidal <adriensg.vidal@gmail.com>
2020-11-22 02:22:18 +0100

    add players sidebar UI

--- client/src/components/sections/Sidebar/index.jsx
+++ client/src/components/sections/Sidebar/index.jsx
@@ -1,16 +1,62 @@
 import React from 'react'
 import style from './index.module.scss'
+import logo from '../../../assets/images/logo.jpg'
 
 const Sidebar = () => {
   return (
     <div className={style.main}>
-      <h2>LimitCoco</h2>
+      <div className={style['logo']}>
+        <img src={logo} alt='logo' />
+      </div>
 
       <div className={style.players}>
+        {/* player */}
         <div className={style.player}>
-          <span className='name'>Player 1</span>
-          <span className='score'>3</span>
-          <span className='turn'>wait</span>
+          <ul>
+            <li className={style.name}>John</li>
+            <li>
+              Score: <span>3</span>
+            </li>
+            <li>
+              Turn: <span>wait</span>
+            </li>
+          </ul>
+        </div>
+        {/* player */}
+        <div className={style.player}>
+          <ul>
+            <li className={style.name}>John</li>
+            <li>
+              Score: <span>3</span>
+            </li>
+            <li>
+              Turn: <span>wait</span>
+            </li>
+          </ul>
+        </div>
+        {/* player */}
+        <div className={style.player}>
+          <ul>
+            <li className={style.name}>John</li>
+            <li>
+              Score: <span>3</span>
+            </li>
+            <li>
+              Turn: <span>wait</span>
+            </li>
+          </ul>
+        </div>
+        {/* player */}
+        <div className={style.player}>
+          <ul>
+            <li className={style.name}>John</li>
+            <li>
+              Score: <span>3</span>
+            </li>
+            <li>
+              Turn: <span>wait</span>
+            </li>
+          </ul>
         </div>
       </div>
     </div>
