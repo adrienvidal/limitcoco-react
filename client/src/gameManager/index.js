0064ba9d5042bc4a2c7a8b129199fd3cc49dc3e6
Adrien Vidal <adriensg.vidal@gmail.com>
2021-02-08 00:50:05 +0100

    .

--- client/src/gameManager/index.js
+++ client/src/gameManager/index.js
@@ -1,38 +1,31 @@
-import { shuffle, deepClone } from "./utils";
-import { v4 as uuidv4 } from "uuid";
-import { questionDeck, answerDeck } from "./cards";
+import { v4 as uuidv4 } from 'uuid'
+import { shuffle, deepClone } from './utils'
+import { questionDeck, answerDeck } from './cards'
 
 export function createNewGame(playerIds) {
+  const answerCards = answerDeck.map((card) => ({
+    id: uuidv4(),
+    text: card.text,
+  }))
 
-  const answerCards = answerDeck.map(card => ({
-      id: uuidv4(),
-      text: card.text
-    })
-  );
+  const questionCards = questionDeck.map((card) => ({
+    id: uuidv4(),
+    text: card.text,
+  }))
 
-  const questionCards = questionDeck.map(card => ({
-      id: uuidv4(),
-      text: card.text
-    })
-  );
+  let questions = shuffle(questionCards)
+  let answers = shuffle(answerCards)
 
-  let questions = shuffle(questionCards);
-  let answers = shuffle(answerCards);
+  const playerHands = {}
 
-  const playerHands = {};
-
-  playerIds.forEach(playerId => {
-    playerHands[playerId] = [
-      answers.pop(),
-      answers.pop(),
-      answers.pop(),
-    ];
-  });
+  playerIds.forEach((playerId) => {
+    playerHands[playerId] = [answers.pop(), answers.pop(), answers.pop()]
+  })
 
   return {
     kingId: playerIds[0],
     players: playerIds,
     hands: playerHands,
-    question: questions[0]
-  };
+    question: questions[0],
+  }
 }
