63314675af08a72c19bf884e865f90d23252ca99
Adrien Vidal <adriensg.vidal@gmail.com>
2021-03-08 20:00:57 +0100

    clean

--- client/src/gameManager/index.js
+++ client/src/gameManager/index.js
@@ -7,19 +7,21 @@ export function createNewGame(playerIds) {
   const kingId = playerIds[0]
 
   // Init Cards
+  // 1-create deck
   const answerCards = answerDeck.map((card) => ({
     id: uuidv4(),
     text: card.text,
   }))
-
   const questionCards = questionDeck.map((card) => ({
     id: uuidv4(),
     text: card.text,
   }))
 
+  // 2-shuffle
   let questions = shuffle(questionCards)
   let answers = shuffle(answerCards)
 
+  // 3-distribute cards to players
   const playerHands = {}
   playerIds.forEach((playerId) => {
     if (playerId !== kingId) {
@@ -34,6 +36,8 @@ export function createNewGame(playerIds) {
   })
 
   return {
+    round: 0,
+    phase: 1,
     kingId: kingId,
     players: playerIds,
     scores: scores,
